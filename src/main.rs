aa36e5bafa7cbb0c8729fe742f4dc615e2ac79ef
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-08-23 12:42:25 +0200

    init commit with hello and get locations endpoint

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+// use std::path::PathBuf;
+use crate::routes::route_all::routes;
+use std::sync::Arc;
+mod routes;
+
+use sqlx::{Postgres, Pool};
+// use tower_http::services::ServeDir;
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[shuttle_runtime::main]
+async fn axum(
+    // Name your static assets folder by passing `folder = <name>` to `StaticFolder`
+    // If you don't pass a name, it will default to `static`.
+    #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    // #[shuttle_static_folder::StaticFolder(folder = "assets")] static_folder: PathBuf,
+) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!().run(&pool).await.expect("Migrations failed :(");
+
+let app = routes(Arc::new(AppState { db: pool.clone() }));
+
+Ok(app.into())
+}
---
+++ src/routes/route_all.rs
@@ -0,0 +1,46 @@
+use std::sync::Arc;
+
+use axum::{
+    routing::get,
+    Router, extract::State, response::IntoResponse, http::StatusCode, Json,
+};
+use sqlx::query_as;
+
+use crate::AppState;
+
+// use crate::{
+//     handlers::charging_station::{
+//         handle_get_all_stations, handle_hello, handle_post, handle_post_a_station,
+//         handler_delete_station_by_id, handler_edit_station_by_id, handler_get_station_by_id,
+//     },
+//     AppState,
+// };
+
+pub async fn handle_hello() -> &'static str {
+    return "Hello, World!";
+}
+
+use serde::{Deserialize, Serialize};
+// use validator::Validate;
+#[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
+pub struct Location {
+    pub id: i32,
+    pub street: String,
+    pub zip: i32,
+    pub city: String,
+    pub country: String,
+}
+
+pub async fn handle_get_all_locations(State(data): State<Arc<AppState>>) -> impl IntoResponse {
+    const QUERY: &str = "SELECT id, name, location, availability FROM locations";
+    let locations: Vec<Location> = query_as(QUERY).fetch_all(&data.db).await.unwrap();
+    println!("\n=== select locations with query.map...: \n{:?}", locations);
+    ((StatusCode::OK), Json(locations))
+}
+
+pub fn routes(app_state: Arc<AppState>) -> Router {
+    Router::new()
+        .route("/hello", get(handle_hello))
+        .route("/api/locations", get(handle_get_all_locations))
+        .with_state(app_state)
+}
