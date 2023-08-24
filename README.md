edc237d0624137d46685e2931d7b7085cf9a98ba
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-08-24 19:40:14 +0200

    add frontend url

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ping-charging-station
+
+https://ping-charging-station.vercel.app - frontend deployed
+https://ping-charging-station.shuttleapp.rs/api/stations
--- shuttle-backend/src/main.rs
+++ shuttle-backend/src/main.rs
@@ -40,7 +40,7 @@ pub async fn axum(
     };
 
     let cors = CorsLayer::new()
-    .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+    .allow_origin("https://ping-charging-station.vercel.app".parse::<HeaderValue>().unwrap())
     .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
     .allow_credentials(true)
     .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
