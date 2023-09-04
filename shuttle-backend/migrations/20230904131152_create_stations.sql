44720958d57b113242ed0ec23d5996179ac0a118
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-09-04 15:12:14 +0200

    update db url and migrations

--- shuttle-backend/migrations/20230904131152_create_stations.sql
+++ shuttle-backend/migrations/20230904131152_create_stations.sql
@@ -2,6 +2,6 @@
 CREATE TABLE stations (
     id  SERIAL PRIMARY KEY,
     name varchar(255) NOT NULL,
-    location varchar(255) NOT NULL,
+    location_id INTEGER REFERENCES locations(id) ON DELETE SET NULL,
     availability BOOLEAN
 );
